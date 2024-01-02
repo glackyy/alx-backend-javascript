8b1da690413b6e420d58a10503e77ddd04db4d91
glackyy <othmanesticky@gmail.com>
2024-01-02 21:44:19 +0100

    10-loops

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,1 +1,7 @@
-#\!/usr/bin/node
+export default function appendToEachArrayValue(array, appendString) {
+  const arrayEnd = [];
+  for (const idx of array) {
+    arrayEnd.push(`${appendString}${idx}`);
+  }
+  return arrayEnd;
+}
