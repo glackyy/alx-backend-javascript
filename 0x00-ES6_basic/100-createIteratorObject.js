fd0d8bde78dfca5ef396af4663cba23ba1c86c4c
glackyy <othmanesticky@gmail.com>
2024-01-02 21:51:00 +0100

    100-createiteratorObj

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,1 +1,7 @@
-#\!/usr/bin/node
+export default function createIteratorObject(report) {
+  const res = [];
+  for (const idx of Object.values(report.allEmployees)) {
+    res.push(...idx);
+  }
+  return res;
+}
