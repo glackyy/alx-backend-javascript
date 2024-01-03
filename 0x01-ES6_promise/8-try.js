4b10181a565275bda1c7c2a211e0131667385fdc
glackyy <othmanesticky@gmail.com>
2024-01-03 11:08:12 +0100

    8-try

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,1 +1,6 @@
-#\!/usr/bin/node
+export default function divideFunction(numerator, deominator) {
+  if (deominator === 0) {
+    throw Error('cannot divide by 0');
+  }
+  return numerator / deominator;
+}
