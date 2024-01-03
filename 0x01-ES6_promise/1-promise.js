3c527ad20f1bee777b37bf151a2ba41cb01124b0
glackyy <othmanesticky@gmail.com>
2024-01-03 10:33:43 +0100

    1-promise

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,1 +1,10 @@
-#\!/usr/bin/node
+export default function getFullResponseFromAPI(success) {
+  const ar = new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+  return ar;
+}
