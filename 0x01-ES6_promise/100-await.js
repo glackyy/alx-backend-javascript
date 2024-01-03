c38f9cf8e6eb6569a3a5436f477395d01e625ab0
glackyy <othmanesticky@gmail.com>
2024-01-03 11:18:53 +0100

    100-await

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,1 +1,12 @@
-#\!/usr/bin/node
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    const p1 = await uploadPhoto();
+    const p2 = await createUser();
+
+    return ({ photo: p1, user: p2 });
+  } catch (error) {
+    return ({ photo: null, user: null });
+  }
+}
