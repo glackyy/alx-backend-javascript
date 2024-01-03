5b670a31dbeeae88f0e7168691678dc53548ca77
glackyy <othmanesticky@gmail.com>
2024-01-03 10:43:18 +0100

    3-all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,1 +1,11 @@
-#\!/usr/bin/node
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const prom1 = uploadPhoto();
+  const prom2 = createUser();
+
+  return Promise.all([prom1, prom2]).then((value) => {
+    console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
+  })
+    .catch(() => { console.log('Signup system offline'); });
+}
