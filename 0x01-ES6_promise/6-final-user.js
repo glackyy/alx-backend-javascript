33fa8912d34e71c7546cc72fa9fd413323392e63
glackyy <othmanesticky@gmail.com>
2024-01-03 10:59:01 +0100

    6-final-user

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,1 +1,19 @@
-#\!/usr/bin/node
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpUserProm = signUpUser(firstName, lastName);
+  const uploadPhotoProm = uploadPhoto(fileName);
+
+  return Promise.allSettled([signUpUserProm, uploadPhotoProm]).then((values) => {
+    const res = [];
+    values.forEach((e) => {
+      if (e.status === 'fulfilled') {
+        res.push({ status: e.status, value: e.value });
+      } else {
+        res.push({ status: e.status, value: `${e.reason}` });
+      }
+    });
+    return res;
+  });
+}
