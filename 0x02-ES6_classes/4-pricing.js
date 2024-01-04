402d778443df3d727cfc43fb322701ca4367d3e3
glackyy <othmanesticky@gmail.com>
2024-01-04 11:09:58 +0100

    4-pricing

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,1 +1,30 @@
-#\!/usr/bin/node
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(at) {
+    this._amount = at;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(cur) {
+    this._currency = cur;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
