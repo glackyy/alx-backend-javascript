5e2162d32a6ced41205003a17103828b986d1adb
glackyy <othmanesticky@gmail.com>
2024-01-04 10:37:05 +0100

    1-make_classroom

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,1 +1,10 @@
-#\!/usr/bin/node
+import ClassRoom from './0-classroom';
+
+export default function initialzeRooms() {
+  const array = [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+  ];
+  return (array);
+}
