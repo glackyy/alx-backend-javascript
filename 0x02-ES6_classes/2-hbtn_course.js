e6be87040c0288d37884217166e5e79114f478ca
glackyy <othmanesticky@gmail.com>
2024-01-04 10:53:36 +0100

    2-htbn_course

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,1 +1,52 @@
-#\!/usr/bin/node
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else {
+      this._name = name;
+    }
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    } else {
+      this._length = length;
+    }
+    if (!Array.isArray(students)) {
+      throw new TypeError('Students must be an array');
+    } else {
+      this._students = students;
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(n) {
+    if (typeof n !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = n;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(len) {
+    if (typeof len !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = len;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(st) {
+    if (!Array.isArray(st)) {
+      throw new TypeError('Students must be an array');
+    }
+    this._students = st;
+  }
+}
