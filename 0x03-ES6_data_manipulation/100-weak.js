249894e0965a15e116988c98f50b98dae3473373
glackyy <othmanesticky@gmail.com>
2024-01-09 08:40:11 +0100

    100-weak

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,1 +1,8 @@
-#\!/usr/bin/node
+export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  let call = 0;
+  if (weakMap.get(endpoint)) call = weakMap.get(endpoint);
+  weakMap.set(endpoint , call + 1);
+  if (call + 1 >= 5) throw new Error('Endpoint load is high');
+}
