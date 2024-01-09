ddf8b311856110eeaf7d220e10ace87ad4b8b4ad
glackyy <othmanesticky@gmail.com>
2024-01-09 08:15:11 +0100

    8-cleanset

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,1 +1,11 @@
-#\!/usr/bin/node
+const cleanSet = (set, startString) => {
+  if (startString === undefined || startString.length === 0) {
+    return '';
+  }
+  return [...set]
+    .filter((param) => (param !== undefined ? param.startsWith(startString) : ''))
+    .map((param) => (param !== undefined ? param.slice(startString.length) : ''))
+    .join('-'');
+};
+
+export default cleanSet;
