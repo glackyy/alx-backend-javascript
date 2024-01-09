7ba292fe94b0fde24f45f002e4d3d3b7eef73f54
glackyy <othmanesticky@gmail.com>
2024-01-09 07:33:29 +0100

    0-getliststudents

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,1 +1,7 @@
-#\!/usr/bin/node
+const getListStudents = () => [
+  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+  { id: 2, firstName: 'James', location: 'Columbia' },
+  { id: 5, firstName: 'Serena', location: 'San Francisco' },
+];
+
+export default getListStudents;
