85eead355900643b67e20587f23945e8e13c0d9f
glackyy <othmanesticky@gmail.com>
2024-01-09 08:02:27 +0100

    5-typedarray

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,1 +1,9 @@
-#\!/usr/bin/node
+export default function createInt8TypedArray(length, position, value) {
+  if (position < 0 || position >= length) {
+    throw Error('Position outside range');
+  }
+  const newB = new ArrayBuffer(length);
+  const int8 = new Int8Array(newB, 0, length);
+  int8.set([value], position);
+  return new DataView(newB);
+}
