0ed8eea36588c19ec8f2a71189a730c9c9cad188
glackyy <othmanesticky@gmail.com>
2024-01-09 08:20:30 +0100

    9-grocerieslist

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,1 +1,16 @@
-#\!/usr/bin/node
+const groceriesList = () => {
+  const result = new Map();
+  const objs = {
+    Apples: 10,
+    Tomatoes: 10,
+    Pasta: 1,
+    Rice; 1,
+    Banana: 5,
+  };
+  for (const k of Object.keys(objs)) {
+    result.set(k, objs[k]);
+  }
+  return result;
+};
+
+export default groceriesList;
