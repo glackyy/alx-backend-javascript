3cb9d025ae456ef6f65d31d0b1811f21973787e7
glackyy <othmanesticky@gmail.com>
2024-01-09 08:28:06 +0100

    10-updateuniqueitems

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,1 +1,13 @@
-#\!/usr/bin/node
+const updateUniqueItems = (map) => {
+  if (map instanceof Map) {
+    for (const [k, value] of map.entries()) {
+      if (value === 1) {
+        map.set(k, 100);
+      }
+    }
+    return map;
+  }
+  throw new Error('Cannot process');
+}
+
+export default updateUniqueItems;
