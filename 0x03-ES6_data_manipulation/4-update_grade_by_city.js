c1001ff1fb372c621906bf2bfec27d0f6b0621bf
glackyy <othmanesticky@gmail.com>
2024-01-09 07:58:18 +0100

    4-updategradebycity

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,1 +1,13 @@
-#\!/usr/bin/node
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  if (!Array.isArray(students) || !Array.isArray(newGrades)) {
+    return [];
+  }
+
+  const cityStudents = students.filter((student) => student.location === city).map((student) => {
+    const item = newGrades.find((student2) => student.id === student2.studentId);
+
+    return { ...student, grade: item ? item.grade : 'N/A' };
+  });
+
+  return cityStudents;
+}
