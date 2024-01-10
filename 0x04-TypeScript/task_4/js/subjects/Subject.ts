6db076091f3a8b96699a8f5de856bef674944293
glackyy <othmanesticky@gmail.com>
2024-01-10 10:28:12 +0100

    js/subjects/Subject.ts

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -1,8 +1,8 @@
 namespace Subjects {
   export class Subject {
-    teacher: Teacher;
-    setTeacher(teacher: Teacher) {
+    teacher: Subjects.Teacher;
+    set setTeacher(teacher: Subjects.Teacher) {
       this.teacher = teacher;
-    };
+    }
   }
 }
