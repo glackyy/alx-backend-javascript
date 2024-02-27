c7bcc5c024da58bbb3f8681d453b107888f8816e
glackyy <othmanesticky@gmail.com>
2024-02-27 12:19:31 +0100

    7-http_express

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,5 +1,19 @@
+const { error } = require('console');
 const express = require('express');
 const { readFile } = require('fs');
 
 const app = express();
 const port = 1245;
+
+function countStudents(fileName) {
+  const students = {};
+  const fields = {};
+  let leng = 0;
+  return new Promise((resolve, reject) => {
+    readFile(fileName, (error, data) => {
+      if (error) {
+        reject(error);
+      }
+    })
+  })
+}
