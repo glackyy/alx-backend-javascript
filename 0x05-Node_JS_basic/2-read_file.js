aeac5e536fce35dab12be031b1c2b100a0fd3e3c
glackyy <othmanesticky@gmail.com>
2024-02-27 10:43:33 +0100

    2-read_file

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+const fs = require('fs');
+
+function countStudents(fileName) {
+  const students = {};
+  const fields = {};
+  let leng = 0;
+  try {
+    const fileContents = fs.readFileSync(fileName, 'utf-8');
+    const lines = fileContents.toString().split('\n');
+  }
+}
