e08da9d5f85864f2ebe4e48b27d6a5348f428461
glackyy <othmanesticky@gmail.com>
2024-02-27 10:55:34 +0100

    3-read_file_async

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,1 +1,17 @@
-#!/usr/bin/node
+const { rejects } = require('assert');
+const { error } = require('console');
+const { readFile } = require('fs');
+const { resolve } = require('path');
+
+function constStudents(fileName) {
+  const students = {};
+  const fields = {};
+  let leng = 0;
+  return new Promise((resolve, reject) => {
+    readFile(fileName, (error, data) => {
+      if (error) {
+        reject(Error('Cannot load the database'));
+      }
+    })
+  }) 
+}
