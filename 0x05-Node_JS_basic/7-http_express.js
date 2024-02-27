b4e77425406fcab1d05a1769ae4d5cfe56e3675c
glackyy <othmanesticky@gmail.com>
2024-02-27 12:23:09 +0100

    7-http_express

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -13,6 +13,15 @@ function countStudents(fileName) {
     readFile(fileName, (error, data) => {
       if (error) {
         reject(error);
+      } else {
+        let output = '';
+        const lines = data.toString().split('\n');
+        for (let i = 0; i < lines.length; i += 1) {
+          if (lines[i]) {
+            leng += 1;
+            const field = lines[i].toString().split(',');
+          }
+        }
       }
     })
   })
