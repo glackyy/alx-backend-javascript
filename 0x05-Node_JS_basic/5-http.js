4ff83e8cdf8034db3cd6116d8f06ccafc1c21e10
glackyy <othmanesticky@gmail.com>
2024-02-27 11:39:20 +0100

    5-http

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -3,3 +3,20 @@ const { readFile } = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 1245;
+
+function countStudents(fileName) {
+  const students = {};
+  const fields = {};
+  let leng = 0;
+  return new Promise((resolve, reject) => {
+    readFile(fileName, (error, data) => {
+      if (error) {
+        reject(error);
+      } else {
+        let output = '';
+        const lines = data.toString().split('\n');
+        for (let i = 0; i < lines.length; i += 1)
+      }
+    })
+  })
+}
