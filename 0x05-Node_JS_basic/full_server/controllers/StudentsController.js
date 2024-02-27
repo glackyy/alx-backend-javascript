be87a71ba1df77d27e843fb8d085e65b92b57e16
glackyy <othmanesticky@gmail.com>
2024-02-27 13:11:40 +0100

    StudentController

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -17,12 +17,16 @@ class StudentsController {
   }
   static getAllStudentsByMajor(request, response) {
     const field = request.params.major;
-    readDatabase(process.argv[2].toString()).then(students) => {
+    readDatabase(process.argv[2].toString()).then((students) => {
       if (!(field in students)) {
         response.status(500).send('Major parameter must be CS or SWE');
       } else {
         response.status(200).send(`List: ${students[field].join(', ')}`);
       }
-    }
+    }).catch(() => {
+      response.status(500).send('Cannot load the database');
+    });
   }
 }
+
+module.exports = StudentsController;
