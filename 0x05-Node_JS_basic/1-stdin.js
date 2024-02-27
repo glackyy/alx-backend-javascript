3b07b871365933998a0bbd21ec847ed4a1c96544
glackyy <othmanesticky@gmail.com>
2024-02-27 10:35:04 +0100

    1-stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,1 +1,8 @@
-#!/usr/bin/node
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+}
