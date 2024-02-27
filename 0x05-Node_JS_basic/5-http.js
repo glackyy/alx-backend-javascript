f79ef430ed8a5ef9f6357f3f1a1923e5d82e9dc1
glackyy <othmanesticky@gmail.com>
2024-02-27 11:43:01 +0100

    5-http

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -15,7 +15,22 @@ function countStudents(fileName) {
       } else {
         let output = '';
         const lines = data.toString().split('\n');
-        for (let i = 0; i < lines.length; i += 1)
+        for (let i = 0; i < lines.length; i += 1) {
+          if (lines[i]) {
+            leng += 1;
+            const field = lines[i].toString().split(',');
+            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+              students[field[3]].push(field[0]);
+            } else {
+              students[field[3]] = [field[0]];
+            }
+            if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+              fields[field[3]] += 1;
+            } else {
+              fields[field[3]] = 1;
+            }
+          }
+        }
       }
     })
   })
