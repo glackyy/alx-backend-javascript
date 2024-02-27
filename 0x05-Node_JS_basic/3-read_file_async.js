767144442085864331758ce44a066b4b36a9264f
glackyy <othmanesticky@gmail.com>
2024-02-27 11:00:05 +0100

    3-read_file_async

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -11,6 +11,26 @@ function constStudents(fileName) {
     readFile(fileName, (error, data) => {
       if (error) {
         reject(Error('Cannot load the database'));
+      } else {
+        const lines = data.toString().split('\n');
+        for (let i = 0; i < lines.length; i += 1) {
+          if (lines[i]) {
+            leng += 1;
+            const field = lines[i].toString().split(',');
+            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+              students[field[3]].push(field[0]);
+            } else {
+              students[field[3]] = [field[0]];
+            }
+            if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+              fields[field[3]] += 1;
+            } else {
+              fields[field[3]] = 1;
+            }
+          }
+        }
+        const l = leng - 1;
+        
       }
     })
   }) 
