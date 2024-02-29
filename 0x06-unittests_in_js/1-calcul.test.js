e97b2f1c38cc0bc10e6bea1688b0bab473689e62
glackyy <othmanesticky@gmail.com>
2024-02-29 11:16:22 +0100

    1-calcul.test

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -3,5 +3,8 @@ const { it, describe } = require("mocha");
 const calculateNumber = require("./1-calcul");
 
 describe("calculateNumber()", function() {
-  
+  it(`checking if numbers round`, function() {
+    const res = calculateNumber("SUM", 1, 2);
+    assert.strictEqual(res, 3);
+  });
 })
