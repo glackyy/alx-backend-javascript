5945a40ed68eea56e20317ff152fabc05f7c48ac
glackyy <othmanesticky@gmail.com>
2024-02-29 11:55:35 +0100

    3-payment.test

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,1 +1,5 @@
-#!/usr/bin/node
+const { describe, it} = require("mocha");
+const sinon = require("sinon");
+const sendPaymentRequestToApi = require("./3-payment");
+const Utils = require("./utils");
+const assert = require("assert");
