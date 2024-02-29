7c93d7f827938fee5f7e7f61d44ef225dddff688
glackyy <othmanesticky@gmail.com>
2024-02-29 15:18:01 +0100

    10-api

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -3,12 +3,25 @@ const express = require('express');
 const app = express();
 const port = 7865;
 
+app.use(express.json());
+
 app.get('/', (request, response) => {
   response.send('Welcome to the payment system');
 });
 
 app.get('/cart/:id([0-9]+)', (request, response) => {
     response.send(`Payment methods for cart ${request.params.id}`);
+});
+
+app.get('/available_payments', (request, response) => {
+  response.set("Content-Type", "application/json");
+  const payMethods = {
+    payment_methods: {
+      credit_cards: true,
+      paypal: false
+    }
+  }
+  response.send(payMethods);
 });
 
 app.listen(port, () => {
