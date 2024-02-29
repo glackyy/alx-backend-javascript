a002a44977d3061675d799526fe256944c1e31dd
glackyy <othmanesticky@gmail.com>
2024-02-29 12:09:13 +0100

    5-payment.test

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,1 +1,8 @@
-#!/usr/bin/node
+const { describe, it } = require("mocha");
+const sinon = require("sinon");
+const sendPaymentRequestToApi = require("./5-payment");
+const expect = require("chai").expect;
+
+describe("sendPaymentRequestToApi", function() {
+  
+});
