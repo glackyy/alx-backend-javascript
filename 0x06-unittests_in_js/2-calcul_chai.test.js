f504651b70eeed98230a1a4255ff123856126d95
glackyy <othmanesticky@gmail.com>
2024-02-29 11:37:20 +0100

    2-calculchai

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -3,28 +3,36 @@ const { it, describe } = require("mocha");
 const calculateNumber = require("./2-calcul_chai");
 
 describe("calculateNumber()", function() {
-  it(`checking if numbers round`, function() {
-    const res = calculateNumber("SUM", 1, 2);
-    assert.strictEqual(res, 3);
-  });
-  it(`checking if numbers round`, function() {
-    const res = calculateNumber("SUBTRACT", 1.4, 2.2);
-    assert.strictEqual(res, -1)
-  });
-  it(`checking if numbers round`, function() {
-    const res = calculateNumber("SUBTRACT", 4.9, 2.7);
-    assert.strictEqual(res, 2);
-  });
-  it(`checking if numbers round`, function() {
-    const res = calculateNumber("DIVIDE", 4, 2);
-    assert.strictEqual(res, 2);
-  });
-  it(`checking if numbers round`, function() {
-    const res = calculateNumber("DIVIDE", 1.7, 0);
-    assert.strictEqual(res, "Error");
-  });
-  it(`checking if numbers round`, function() {
-    const res = calculateNumber("DIVIDE", 1.4, 4.6);
-    assert.strictEqual(res, 0.2);
-  });
+  describe("SUM", function() {
+    it(`checking if numbers round`, function() {
+      expect(calculateNumber("SUM", 1, 2)).to.equal(3);
+    });
+    it(`checking if numbers round`, function() {
+      expect(calculateNumber("SUM", 1.6, 2.6)).to.equal(5);
+    });
+  });  
+  describe("SUM", function() {
+    it(`checking if numbers round`, function() {
+      expect(calculateNumber("SUM", 1, 2)).to.equal(3);
+    });
+    it(`checking if numbers round`, function() {
+      expect(calculateNumber("SUM", 1.6, 2.6)).to.equal(5);
+    });
+  });  
+  describe("SUM", function() {
+    it(`checking if numbers round`, function() {
+      expect(calculateNumber("SUM", 1, 2)).to.equal(3);
+    });
+    it(`checking if numbers round`, function() {
+      expect(calculateNumber("SUM", 1.6, 2.6)).to.equal(5);
+    });
+  });  
+  describe("SUM", function() {
+    it(`checking if numbers round`, function() {
+      expect(calculateNumber("SUM", 1, 2)).to.equal(3);
+    });
+    it(`checking if numbers round`, function() {
+      expect(calculateNumber("SUM", 1.6, 2.6)).to.equal(5);
+    });
+  });  
 });
