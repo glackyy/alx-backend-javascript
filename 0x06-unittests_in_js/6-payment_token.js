a0deb3261d99f44dc723b3bcb40b93156ee31d89
glackyy <othmanesticky@gmail.com>
2024-02-29 12:44:01 +0100

    6-payment_token

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,1 +1,9 @@
-#!/usr/bin/node
+function getPaymentTokenFromAPI(success) {
+  if (success) {
+    return new Promise((resolve) => {
+      resolve({ data: 'Successful response from the API' });
+    });
+  }
+}
+
+module.exports = getPaymentTokenFromAPI;
