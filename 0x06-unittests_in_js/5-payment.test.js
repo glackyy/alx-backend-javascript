cc5e1471b7d532071d6cdc217b53f61a85fa7d8a
glackyy <othmanesticky@gmail.com>
2024-02-29 12:10:00 +0100

    5-payment.test

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -4,5 +4,7 @@ const sendPaymentRequestToApi = require("./5-payment");
 const expect = require("chai").expect;
 
 describe("sendPaymentRequestToApi", function() {
-  
+  beforeEach("Setting up spy to use for each test", function() {
+    sinon.spy(console, "log");
+  });
 });
