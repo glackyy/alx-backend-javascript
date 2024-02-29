4657417460fc5e994be060668f73176713b955f9
glackyy <othmanesticky@gmail.com>
2024-02-29 11:02:19 +0100

    0-calcul.test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -7,4 +7,8 @@ describe("calculateNumber()", function() {
     const res = calculateNumber(1, 2);
     assert.strictEqual(res, 3);
   });
+  it(`checking if numbers round`, function() {
+    const res = calculateNumber(1.4, 2.2);
+    assert.strictEqual(res, 3);
+  });
 })
