32fccfc88639be7f068a930daf6ad44058709a8e
glackyy <othmanesticky@gmail.com>
2024-02-29 14:23:24 +0100

    8-api

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,1 +1,7 @@
-#!/usr/bin/node
+const express = require("express");
+const app = express();
+const port = 7865;
+
+app.get('/', (request, response) => {
+  response.send('Welcome to the payment system');
+});
