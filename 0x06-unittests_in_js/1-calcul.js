9aadfce98e963881f6e75e1b3a91c175c3b596a9
glackyy <othmanesticky@gmail.com>
2024-02-29 11:09:28 +0100

    1-calcul

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+function calculateNumber(type, a, b) {
+  const an = Math.round(a);
+  const bn = Math.round(b);
+  let c = 0;
+  switch (type) {
+    case 'SUM':
+        c = an + bn;
+    case 'SUBSTRACT':
+        c = an - bn;
+  }
+}
