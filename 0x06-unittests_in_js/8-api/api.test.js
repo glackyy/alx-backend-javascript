547be9f2bf99d0e6ecd117c7126ead56777d57a4
glackyy <othmanesticky@gmail.com>
2024-02-29 14:31:40 +0100

    8-api

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -3,5 +3,14 @@ const { it, describe } = require("mocha");
 const expect = require("chai").expect;
 
 describe("Index page", function() {
-  
+  const options = {
+    url: "http://localhost:7865/",
+    method: "GET"
+  }
+  it(`checking correct status code`, function(done) {
+    request(options, function(error, res, body) {
+      expect(res.statusCode).to.equal(200);
+      done();
+    });
+  });
 });
