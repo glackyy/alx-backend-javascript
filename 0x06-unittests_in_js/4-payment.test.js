98ca6515592b2f2c3f9fda972444cb3f67da22d9
glackyy <othmanesticky@gmail.com>
2024-02-29 12:06:01 +0100

    4-payment.test

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,1 +1,15 @@
-#!/usr/bin/node
+const { describe, it} = require("mocha");
+const sinon = require("sinon");
+const sendPaymentRequestToApi = require("./4-payment");
+const Utils = require("./utils");
+const assert = require("assert");
+
+describe("sendPaymentRequestToApi", function() {
+  it(`checking if Utils.calculateNumber is stubbed`, function() {
+    const spy = sinon.spy(console, "log");
+    const stub = sinon.stub(Utils, "calculateNumber").returns(10);
+    sendPaymentRequestToApi(100, 20);
+    assert(spy.withArgs("The total is: 10").calledOnce);
+    assert(stub.withArgs("SUM", 100, 20).calledOnce);
+  });
+});
