10c16e8d08b5110750e57a4371ffcbcad7c44b16
glackyy <othmanesticky@gmail.com>
2024-02-29 11:15:14 +0100

    1-calcul.test + adding files

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,1 +1,7 @@
-#!/usr/bin/node
+const assert = require("assert");
+const { it, describe } = require("mocha");
+const calculateNumber = require("./1-calcul");
+
+describe("calculateNumber()", function() {
+  
+})
