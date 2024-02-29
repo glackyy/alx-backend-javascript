ee7a944153c43432c0e613f80fef223340137215
glackyy <othmanesticky@gmail.com>
2024-02-29 20:09:51 +0100

    10-api

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -1,23 +1,27 @@
 const request = require("request");
-const { describe, it } = require("mocha");
+const { it, describe } = require("mocha");
 const expect = require("chai").expect;
 
-describe("Index page", function () {
+describe("Index page", function() {
   const options = {
     url: "http://localhost:7865/",
-    method: "GET",
-  };
-
-  it("check correct status code", function (done) {
-    request(options, function (err, res, body) {
+    method: "GET"
+  }
+  it(`checking correct status code`, function(done) {
+    request(options, function(error, res, body) {
       expect(res.statusCode).to.equal(200);
       done();
     });
   });
-
-  it("check correct content", function (done) {
-    request(options, function (err, res, body) {
-      expect(body).to.equal("Welcome to the payment system");
+  it(`checking correct content`, function(done) {
+    request(options, function(error, res, body) {
+      expect(body).to.contain("Welcome to the payment system");
+      done();
+    });
+  });
+  it(`checking correct content length`, function(done) {
+    request(options, function(error, res, body) {
+      expect(res.headers['content-length']).to.equal('29');
       done();
     });
   });
@@ -39,7 +43,7 @@ describe("Cart page", function () {
   });
 
   it("check correct status code for incorrect url", function (done) {
-    request.get("http://localhost:7865/cart/kim", function (err, res, body) {
+    request.get("http://localhost:7865/cart/ok", function (err, res, body) {
       expect(res.statusCode).to.equal(404);
       done();
     });
