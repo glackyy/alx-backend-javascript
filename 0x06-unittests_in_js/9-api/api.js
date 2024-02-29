433bcc384566451d6b29f6d0cfeab5b887003539
glackyy <othmanesticky@gmail.com>
2024-02-29 14:53:01 +0100

    9-api

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -6,8 +6,12 @@ app.get('/', (request, response) => {
   response.send('Welcome to the payment system');
 });
 
+app.get('/cart/:id[0-9]+', (request, response) => {
+  response.send(`Payment methods for cart ${request.params.id}`);
+});
+
 app.listen(port, () => {
-  console.log("API available on localhost port 7865");
+  console.log("API available on localhost port ${port}");
 });
 
 module.exports = app;
