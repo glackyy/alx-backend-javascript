30d24d0d61d7007bc4e477285e204d5fa872cfcf
glackyy <othmanesticky@gmail.com>
2024-02-29 11:40:01 +0100

    2-calculchai

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -19,12 +19,15 @@ describe("calculateNumber()", function() {
       expect(calculateNumber("SUBTRACT", 4.9, 2.7)).to.equal(2);
     });
   });  
-  describe("SUM", function() {
+  describe("DIVIDE", function() {
     it(`checking if numbers round`, function() {
-      expect(calculateNumber("SUM", 1, 2)).to.equal(3);
+      expect(calculateNumber("DIVIDE", 4, 2)).to.equal(2);
     });
     it(`checking if numbers round`, function() {
-      expect(calculateNumber("SUM", 1.6, 2.6)).to.equal(5);
+      expect(calculateNumber("DIVIDE", 4.6, 1.8)).to.equal(2.5);
+    });
+    it(`checking if numbers round`, function() {
+      expect(calculateNumber("DIVIDE", 4, 0)).to.equal("Error");
     });
   });   
 });
